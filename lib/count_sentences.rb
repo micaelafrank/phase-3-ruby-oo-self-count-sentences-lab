e02b86cafa098f056953e38da63a82ed6f1bbe1b
Micaela Frank <micaelalfrank@gmail.com>
2022-06-08 12:43:00 -0400

    commit tree 840924ff754baadd6011dd35c30e31a00fd9d1c4

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter { |item| !item.empty? }.size
   end
 end
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
